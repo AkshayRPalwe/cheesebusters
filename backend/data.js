1fe70dc8da8434fa927c613aeb5783a46a70e03b
Akshay Palwe <akshaypalwe@Akshays-MacBook-Air.local>
2021-09-25 09:54:19 +0530

    data updated

--- backend/data.js
+++ backend/data.js
@@ -20,106 +20,114 @@ const data = {
     {
       /* we are going to define products array using square bracket, inside that create product object */
 
-      name: "iphone 12 mini",
-      category: "Mobiles",
-      image: "/images/1.jpeg" /* JSON object literals */,
-      price: 60000,
+      name: "Maharaja Veg Cheese Burst Pizza [Medium]",
+      category: "Pizza",
+      image: "/images/c13.jpg" /* JSON object literals */,
+      price: 700,
       countInStock: 10,
-      brand: "Apple",
+      brand: "CheeseBusters",
       rating: 4.5,
       numReviews: 10,
-      description: "High quality product",
+      description:
+        "Indulgent and oh-so-cheesy! Our special tribute for the king - adorned with Spiced paneer tikka, jalapenos, bell peppers, marinated olives and onion - hand-tossed 11-inch crust",
     },
     {
       /* we are going to define products array using square bracket, inside that create product object */
 
-      name: "iphone 12",
-      category: "Mobiles",
-      image: "/images/p2.jpeg" /* JSON object literals */,
-      price: 70000,
+      name: "Caramelized Onion Cheese Burst Pizza",
+      category: "Pizza",
+      image: "/images/c12.jpg" /* JSON object literals */,
+      price: 450,
       countInStock: 10,
-      brand: "Apple",
+      brand: "CheeseBusters",
       rating: 4.2,
       numReviews: 15,
-      description: "High quality product",
+      description:
+        "Loaded with crispy, subtly sweet, golden caramelized onions smothered in barbeque sauce, bursting with molten cheesy lava! Relish this cheesy goodness on a soft and herby 11-inch crust.",
     },
     {
       /* we are going to define products array using square bracket, inside that create product object */
 
-      name: "iphone 12 Pro",
-      category: "Mobiles",
-      image: "/images/p3.jpeg" /* JSON object literals */,
-      price: 90000,
+      name: "Chicken Tikka Cheese Burst Pizza",
+      category: "Pizza",
+      image: "/images/c11.jpg" /* JSON object literals */,
+      price: 500,
       countInStock: 10,
-      brand: "Apple",
+      brand: "CheeseBusters",
       rating: 4.6,
       numReviews: 20,
-      description: "High quality product",
+      description:
+        "Tender, flavourful chunks of delicious tandoori chicken, on a bed of creamy cheese waiting to explode in your mouth! Grab this cheesy wonder on a soft and herby 11-inch crust.",
     },
     {
       /* we are going to define products array using square bracket, inside that create product object */
 
-      name: "iphone 12 Pro Max",
-      category: "Mobiles",
-      image: "/images/p4.jpeg",
-      price: 1200000,
+      name: "Corn and Cheese Cheese Burst Pizza",
+      category: "Pizza",
+      image: "/images/c10.jpg",
+      price: 400,
       countInStock: 0,
-      brand: "Apple",
+      brand: "CheeseBusters",
       rating: 4.9,
       numReviews: 40,
-      description: "High quality product",
+      description:
+        "Juicy kernels of American corn with a burst of creamy, molten cheese in every bite. Enjoy this creamy indulgence on a soft and herby 11-inch crust",
     },
     {
       /* we are going to define products array using square bracket, inside that create product object */
 
-      name: "iphone 11",
-      category: "Mobiles",
-      image: "/images/p5.jpeg",
-      price: 45000,
+      name: "Double Cheese Margherita Cheese Burst Pizza",
+      category: "Pizza",
+      image: "/images/c9.jpg",
+      price: 450,
       countInStock: 15,
-      brand: "Apple",
+      brand: "CheeseBusters",
       rating: 4.3,
       numReviews: 50,
-      description: "High quality product",
+      description:
+        "Cheese the day with our double cheese Margherita pizza, coupled with melt-in-the-mouth creamy cheese inside! Nothing cant get cheesier than this wonder on a herby and soft 11-inch crust.",
     },
     {
       /* we are going to define products array using square bracket, inside that create product object */
 
-      name: "iphone 11 Pro",
-      category: "Mobiles",
-      image: "/images/p6.jpeg",
-      price: 70000,
+      name: "Mutton Overload Cheese Burst Pizza [Medium]",
+      category: "Pizza",
+      image: "/images/c8.jpg",
+      price: 750,
       countInStock: 0,
-      brand: "Apple",
+      brand: "CheeseBusters",
       rating: 4.7,
       numReviews: 5,
-      description: "High quality product",
+      description:
+        "A burst of cheese in every mouthful! Its a true delight for all meat lovers with Succulent Lamb Chunks and spicy, delicious Chicken Keema with Mozzarella Cheese on a soft 11-inch crust.",
     },
     {
       /* we are going to define products array using square bracket, inside that create product object */
 
-      name: "iphone 11 Pro Max",
-      category: "Mobiles",
-      image: "/images/p7.jpeg",
-      price: 95000,
+      name: "Non-Veg Paradise Cheese Burst Pizza [Medium]",
+      category: "Pizza",
+      image: "/images/c1.jpg",
+      price: 700,
       countInStock: 2,
-      brand: "Apple",
+      brand: "CheeseBusters",
       rating: 4.6,
       numReviews: 25,
-      description: "High quality product",
+      description:
+        "Caution: Melts-in-the-mouth! Enjoy this delicious chicken fiesta, loaded with Chicken Tikka, Sliced Chicken Meatballs, Chicken Sausages, Onion & Mozzarella Cheese on a soft 11-inch crust.",
     },
     {
       /* we are going to define products array using square bracket, inside that create product object */
 
-      name: "iphone SE",
-      category: "Mobiles",
-      image: "/images/p8.jpeg",
-      price: 35000,
+      name: "Famous Five Cheese Burst Pizza [Medium]",
+      category: "Pizza",
+      image: "/images/c5.jpg",
+      price: 600,
       countInStock: 0,
-      brand: "Apple",
+      brand: "CheeseBusters",
       rating: 3.8,
       numReviews: 145,
-      description: "High quality product",
+      description:
+        "Gooey deliciousness for your cheese-tooth! Relish our awesome five-some Tender Paneer Tikka, Jalapenos, Black Olives, American Corn & Onion with Mozzarella Cheese on a soft 11-inch crust.",
     },
   ],
 };
